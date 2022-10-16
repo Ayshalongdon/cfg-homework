74d190c373c42bce25af238e051d5c1ec68aece8
Aysha Longdon <ayshalongdon@gmail.com>
2022-10-16 17:22:25 +0100

    Almost finished homework 1, few changes to be made

--- homework1/cash_register.py
+++ homework1/cash_register.py
@@ -3,7 +3,7 @@
 TASK 1
 
 Write a class to represent a Cash Register.
-You class should keep the state of price total and purchased items
+Your class should keep the state of price total and purchased items
 
 Below is a starter code:
 ------------------------
@@ -19,29 +19,52 @@ class CashRegister:
 
     def __init__(self):
 
-        self.total_items = None # {'item': 'price'}
+        self.total_items = {}
         self.total_price = 0
         self.discount = 0
 
-    def add_item(self):
-        pass
+    def add_item(self, item, price):
+        self.total_items.update({item: price})
+        return self.total_items
 
-    def remove_item(self):
-        pass
+    def remove_item(self, item):
+        self.total_items.pop(item)
+        return self.total_items
 
     def apply_discount(self):
-        pass
+        discount_amount = sum(self.total_items.values()) - self.discount
+        print(f"Your discount is: £{discount_amount}.")
+        return discount_amount
 
     def get_total(self):
-        pass
+        total_price = sum(self.total_items.values()) - self.discount
+        print(f"Your total amount is £{total_price}")
+        return total_price
 
     def show_items(self):
-        pass
+        print(f"Your items are {self.total_items}")
+        return self.total_items
 
     def reset_register(self):
-        pass
+        self.total_items.clear()
+        self.discount = 0
+        self.total_price = 0
+        print(f"Transaction cleared")
 
 
 # EXAMPLE code run:
+shopping_trolley = CashRegister()
 
-# ADD
+shopping_trolley.add_item('jeans', 12.99)
+shopping_trolley.add_item('jumper', 15.99)
+shopping_trolley.show_items()
+shopping_trolley.remove_item('jumper')
+shopping_trolley.show_items()
+shopping_trolley.reset_register()
+shopping_trolley.show_items()
+shopping_trolley.add_item('perfume', 49.99)
+shopping_trolley.add_item('moisturiser', 50.99)
+shopping_trolley.show_items()
+shopping_trolley.get_total()
+shopping_trolley.apply_discount()
+shopping_trolley.get_total()
--- homework1/student.py
+++ homework1/student.py
@@ -18,10 +18,37 @@ class Student:
         self.name = name
         self.age = age
         self.id = id
-        self.subjects = dict()
+        self.subjects = {}
 
+    def student_details(self):
+        return print(f"Student name is{self.name}, age is {self.age}, ID is {self.id}.")
+
+    def add_subject(self, subject, grade):
+        self.subjects.update({subject: grade})
+        return self.subjects
+
+    def remove_subjects(self, subject):
+        self.subjects.pop(subject)
+        return self.subjects
+
+    def view_subjects(self):
+        print(f"The student {self.name} is studying {self.subjects}.")
+        return self.subjects
+
+    def calculate_marks(self):
+        total_marks = sum(self.subjects.values())
+        total_subjects = len(self.subjects)
+        average_marks = total_marks / total_subjects
+        print(f"The average mark of {self.name} is {average_marks}.")
+        return average_marks
+
+
+cfg_student = Student
+
+cfg_student.student_details('Aysha', 25, 'ABC')
+cfg_student.add_subject('Software', 99)
 
 # class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subects (add/remove new subject and its graade to the dict)
+#     create new methods that manage student's subjects (add/remove new subject and its grade to the dict)
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
